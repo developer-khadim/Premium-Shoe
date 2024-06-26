742b715f0e19264185e8226d5fa83e7984c9cc00
Your Name <you@example.com>
2024-06-27 02:53:27 +0500

    pic

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,4 +1,6 @@
 "use client";
+
+import logo from '../src/assets/brand_logo.png';
 import React from 'react';
 
 const Nav = () => {
@@ -6,7 +8,7 @@ const Nav = () => {
     <div className='mt-2 w-full h-auto sm:h-[50px] flex justify-center items-center'>
       <nav className='w-full px-4 sm:w-[90vw] h-auto sm:h-[30px] flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0'>
         <div className="w-full sm:w-auto flex justify-center sm:justify-start">
-          <img src="./src/assets/brand_logo.png" alt="Brand Logo" className="h-8 sm:h-auto" />
+          <img src={logo} alt="Brand Logo" className="h-8 sm:h-auto" />
         </div>
         <div className='text-[13px] flex flex-wrap justify-center gap-3 uppercase font-semibold'>
            <h3 className="hover:text-red-500 cursor-pointer">Menu</h3>
